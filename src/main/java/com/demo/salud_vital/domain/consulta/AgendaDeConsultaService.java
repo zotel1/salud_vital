7336993fd77d4bc0f0b50e38c2c4369167be25ce
zotel1 <zotelsigel@gmail.com>
2024-09-12 17:20:40 -0300

    update v6

--- src/main/java/com/demo/salud_vital/domain/consulta/AgendaDeConsultaService.java
+++ src/main/java/com/demo/salud_vital/domain/consulta/AgendaDeConsultaService.java
@@ -48,13 +48,13 @@ public class AgendaDeConsultaService {
 
         var paciente = pacienteRepository.findById(datos.idPaciente()).get();
 
-        var medico = seleccionarMedico(datos);
+        var medico = medicoRepository.findById(datos.idMedico());
 
-        if(medico==null){
+        if(medico.isEmpty()){
             throw new ValidacionDeIntegridad("no existen medicos disponibles para este horario y especialidad");
         }
 
-        var consulta = new Consulta(medico,paciente,datos.fecha());
+        var consulta = new Consulta(medico.get(),paciente,datos.fecha());
 
         consultaRepository.save(consulta);
 
@@ -73,20 +73,20 @@ public class AgendaDeConsultaService {
         consulta.cancelar(datos.motivo());
     }
 
-    private Medico seleccionarMedico(DatosAgendarConsulta datos) {
+    //private Medico seleccionarMedico(DatosAgendarConsulta datos) {
 
        // Pageable page = PageRequest.of(0, 1, Sort.by("rand()"));
 
-        if(datos.idMedico()!=null){
-            return medicoRepository.getReferenceById(datos.idMedico());
-        }
-        if(datos.especialidad()==null){
-            throw new ValidacionDeIntegridad("debe seleccionarse una especialidad para el medico");
-        }
+      //  if(datos.idMedico()!=null){
+        //    return medicoRepository.getReferenceById(datos.idMedico());
+      //  }
+      //  if(datos.especialidad()==null){
+        //    throw new ValidacionDeIntegridad("debe seleccionarse una especialidad para el medico");
+       // }
 
-        return medicoRepository.seleccionarMedicoConEspecialidadEnFecha(datos.especialidad(),datos.fecha());
+       // return medicoRepository.seleccionarMedicoConEspecialidadEnFecha(datos.especialidad(),datos.fecha());
        //return medicoRepository.seleccionarMedicoConEspecialidadEnFecha(datos.especialidad(),datos.fecha()).getContent().get(0);
-    }
+   // }
 
     public Page<DatosDetalleConsulta> consultar(Pageable paginacion) {
         return consultaRepository.findAll(paginacion).map(DatosDetalleConsulta::new);
