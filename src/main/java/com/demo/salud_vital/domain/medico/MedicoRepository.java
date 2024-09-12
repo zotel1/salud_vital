268e03728f8ff0f994175111271f90a0c4653430
zotel1 <zotelsigel@gmail.com>
2024-09-12 17:08:37 -0300

    update v4

--- src/main/java/com/demo/salud_vital/domain/medico/MedicoRepository.java
+++ src/main/java/com/demo/salud_vital/domain/medico/MedicoRepository.java
@@ -14,23 +14,21 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findByActivoTrue(Pageable paginacion);
 
 
-    @Query("""
-            select * from Medico m
-            where m.activo = 1 
-            and
-            m.especialidad_id=:especialidad 
-            and
-            m.id not in(  
-                select c.medico_id from Consulta c
-                where
-                c.fecha=:fecha
-            )
-            order by rand()
-            limit 1
-            """)
-    Medico seleccionarMedicoConEspecialidadEnFecha(@Param("especialidadId") Long especialidad, @Param("fecha") LocalDateTime fecha);
-//Medico seleccionarMedicoConEspecialidadEnFecha( Especialidad especialidad, LocalDateTime fecha);
-//
+//    @Query("""
+  //          select m from Medico m
+    //        where m.activo = 1
+      //      and
+        //    m.especialidad=:especialidad
+//            and
+  //          m.id not in(
+    //            select c.medico.id from Consulta c
+      //          where
+        //        c.fecha=:fecha
+   //         )
+     //       order by rand()
+    //        limit 1
+     //       """)
+  //  Medico seleccionarMedicoConEspecialidadEnFecha(Especialidad especialidad, LocalDateTime fecha);
 //    Page<Medico> seleccionarMedicoConEspecialidadEnFecha(Especialidad especialidad, LocalDateTime fecha);
 
 
