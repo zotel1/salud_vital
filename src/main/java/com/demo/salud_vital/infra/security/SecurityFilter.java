98da0ed4bcf2658939d24ef32c08449b50820a51
zotel1 <zotelsigel@gmail.com>
2024-09-12 15:00:41 -0300

    update comentamos SecurityFilter

--- src/main/java/com/demo/salud_vital/infra/security/SecurityFilter.java
+++ src/main/java/com/demo/salud_vital/infra/security/SecurityFilter.java
@@ -17,29 +17,29 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
-public class SecurityFilter extends OncePerRequestFilter {
+//@Component
+//public class SecurityFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private TokenService tokenService;
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+  //  @Autowired
+    //private TokenService tokenService;
+   // @Autowired
+   // private UsuarioRepository usuarioRepository;
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    //@Override
+   // protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // Obtener el token del header
-        var authHeader = request.getHeader("Authorization");
-        if (authHeader != null) {
-            var token = authHeader.replace("Bearer ", "");
-            var nombreUsuario = tokenService.getSubject(token); // extract username
-            if (nombreUsuario != null) {
+     //   var authHeader = request.getHeader("Authorization");
+     //   if (authHeader != null) {
+       //     var token = authHeader.replace("Bearer ", "");
+       //     var nombreUsuario = tokenService.getSubject(token); // extract username
+        //    if (nombreUsuario != null) {
                 // Token valido
-                var usuario = usuarioRepository.findByLogin(nombreUsuario);
-                var authentication = new UsernamePasswordAuthenticationToken(usuario, null,
-                        usuario.getAuthorities()); // Forzamos un inicio de sesion
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
-        }
-        filterChain.doFilter(request, response);
-    }
-}
+         //       var usuario = usuarioRepository.findByLogin(nombreUsuario);
+          //      var authentication = new UsernamePasswordAuthenticationToken(usuario, null,
+            //            usuario.getAuthorities()); // Forzamos un inicio de sesion
+           //     SecurityContextHolder.getContext().setAuthentication(authentication);
+         //   }
+      //  }
+     //   filterChain.doFilter(request, response);
+   // }
+//}
