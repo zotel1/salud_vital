e96396eb3e11e6b9023322dcc5b95638a3ccb6f0
zotel1 <zotelsigel@gmail.com>
2024-09-14 14:51:21 -0300

    update v18 cors sin auth0

--- src/main/java/com/demo/salud_vital/domain/usuarios/Auth0UserSyncFilter.java
+++ src/main/java/com/demo/salud_vital/domain/usuarios/Auth0UserSyncFilter.java
@@ -1,35 +1,26 @@
 package com.demo.salud_vital.domain.usuarios;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
 
 
-@Component
-public class Auth0UserSyncFilter extends OncePerRequestFilter {
 
-    private final UsuarioService usuarioService;
 
-    public Auth0UserSyncFilter(UsuarioService usuarioService) {
-        this.usuarioService = usuarioService;
-    }
+//@Component
+//public class Auth0UserSyncFilter extends OncePerRequestFilter {
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+  //  private final UsuarioService usuarioService;
+
+//    public Auth0UserSyncFilter(UsuarioService usuarioService) {
+  //      this.usuarioService = usuarioService;
+//    }
+
+  //  @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+  //          throws ServletException, IOException {
         // Obtener la autenticación actual (token JWT)
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-            String email = ((UserDetails) authentication.getPrincipal()).getUsername();
-            usuarioService.sincronizarUsuario(email); // Sincronizar usuario en base de datos
-        }
-    }
-}
+    //    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      //  if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+        //    String email = ((UserDetails) authentication.getPrincipal()).getUsername();
+          //  usuarioService.sincronizarUsuario(email); // Sincronizar usuario en base de datos
+//        }
+  //  }
+//}
