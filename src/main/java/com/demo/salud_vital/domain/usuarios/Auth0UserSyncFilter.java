6c5ddc14dcc84187b30fc7b1342547700e7b0465
zotel1 <zotelsigel@gmail.com>
2024-09-14 14:23:51 -0300

    update v17 cors

--- src/main/java/com/demo/salud_vital/domain/usuarios/Auth0UserSyncFilter.java
+++ src/main/java/com/demo/salud_vital/domain/usuarios/Auth0UserSyncFilter.java
@@ -4,7 +4,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,23 +12,24 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+
 @Component
 public class Auth0UserSyncFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private UsuarioService usuarioService;
+    private final UsuarioService usuarioService;
+
+    public Auth0UserSyncFilter(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        // Obtén el email del usuario autenticado a partir del token JWT
+        // Obtener la autenticación actual (token JWT)
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
             String email = ((UserDetails) authentication.getPrincipal()).getUsername();
-            usuarioService.sincronizarUsuario(email); // Sincroniza el usuario si no está en la base de datos
+            usuarioService.sincronizarUsuario(email); // Sincronizar usuario en base de datos
         }
-        filterChain.doFilter(request, response);
     }
-}
-
 }
