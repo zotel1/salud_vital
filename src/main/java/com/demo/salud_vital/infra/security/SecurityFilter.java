a6d0c01d675f0b543592a89a132460f6473f61b8
zotel1 <zotelsigel@gmail.com>
2024-09-13 21:46:19 -0300

    update v1

--- src/main/java/com/demo/salud_vital/infra/security/SecurityFilter.java
+++ src/main/java/com/demo/salud_vital/infra/security/SecurityFilter.java
@@ -1,21 +1,21 @@
-package com.demo.salud_vital.infra.security;//package com.demo.salud_vital.infra.security;
+//package com.demo.salud_vital.infra.security;//package com.demo.salud_vital.infra.security;
 
 //import com.demo.salud_vital.domain.usuarios.UsuarioRepository;
-import com.demo.salud_vital.domain.usuarios.UsuarioRepository;
-import com.demo.salud_vital.infra.security.TokenService;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+//import com.demo.salud_vital.domain.usuarios.UsuarioRepository;
+//import com.demo.salud_vital.infra.security.TokenService;
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+//import java.io.IOException;
 
 //@Component
 //public class SecurityFilter extends OncePerRequestFilter {
--- src/main/java/com/demo/salud_vital/infra/security/TokenService.java
+++ src/main/java/com/demo/salud_vital/infra/security/TokenService.java
@@ -14,49 +14,49 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
-@Service
-public class TokenService {
+//@Service
+//public class TokenService {
 
-    @Value("${api.security.secret}")
-    private String apiSecret;
+ //   @Value("${api.security.secret}")
+   // private String apiSecret;
 
-    public String generarToken(Usuario usuario) {
-        try {
-            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
-            return JWT.create()
-                    .withIssuer("voll med")
-                    .withSubject(usuario.getLogin())
-                    .withClaim("id", usuario.getId())
-                    .withExpiresAt(generarFechaExpiracion())
-                    .sign(algorithm);
-        } catch (JWTCreationException exception){
-            throw new RuntimeException();
-        }
-    }
+  //  public String generarToken(Usuario usuario) {
+    //    try {
+      //      Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+        //    return JWT.create()
+          //          .withIssuer("voll med")
+            //        .withSubject(usuario.getLogin())
+              //      .withClaim("id", usuario.getId())
+                //    .withExpiresAt(generarFechaExpiracion())
+                  //  .sign(algorithm);
+//        } catch (JWTCreationException exception){
+  //          throw new RuntimeException();
+    //    }
+//    }
 
-    public String getSubject(String token) {
-        if (token == null) {
-            throw new RuntimeException();
-        }
-        DecodedJWT verifier = null;
-        try {
-            Algorithm algorithm = Algorithm.HMAC256(apiSecret); // validando firma
-            verifier = JWT.require(algorithm)
-                    .withIssuer("voll med")
-                    .build()
-                    .verify(token);
-            verifier.getSubject();
-        } catch (JWTVerificationException exception) {
-            System.out.println(exception.toString());
-        }
-        if (verifier.getSubject() == null) {
-            throw new RuntimeException("Verifier invalido");
-        }
-        return verifier.getSubject();
-    }
+  //  public String getSubject(String token) {
+    //    if (token == null) {
+      //      throw new RuntimeException();
+//        }
+ //       DecodedJWT verifier = null;
+   //     try {
+     //       Algorithm algorithm = Algorithm.HMAC256(apiSecret); // validando firma
+       //     verifier = JWT.require(algorithm)
+         //           .withIssuer("voll med")
+           //         .build()
+             //       .verify(token);
+ //           verifier.getSubject();
+   //     } catch (JWTVerificationException exception) {
+     //       System.out.println(exception.toString());
+   //     }
+     //   if (verifier.getSubject() == null) {
+       //     throw new RuntimeException("Verifier invalido");
+ //       }
+   //     return verifier.getSubject();
+ //   }
 
-    private Instant generarFechaExpiracion() {
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
-    }
+   // private Instant generarFechaExpiracion() {
+     //   return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
+//    }
 
-}
+//}
